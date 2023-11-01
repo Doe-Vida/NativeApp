8e484f439b61d542458ea2a2c1a8f6e3d7247814
interview-user <caroline.souzab12@gmail.com>
2023-10-31 22:52:35 -0300

    adding colors and text style

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,39 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        pink:{
+        black: "#000000",
+        white: "#FFFFFF",
+        gray: {
+          10: "#1F1B29",
+          25: "#2E2A36",
+          50: "#616161",
+          100: "#99969E",
+          200: "#BAB9BD",
+          300: "#616161",
+          400: "#ECECEE",
+          500: "#F3F3F5",
+        },
+        pink: {
           primary: "#F50057",
           second: "#EC407A",
+        },
+        blue: {
+          link: "#1A44AF",
+        },
+        cyan: "#80DEEA",
+      },
+      textStyle: {
+        fontSize: {
+          'header1': '25px',
+          'header2': '20px',
+          'header3': '18px',
+          'body1': '16px',
+          'body2': '14px',
+          'body3': '12px',
+          'body4': '10px',
+        },
+        fontFamily: {
+          'sans': ['Roboto', 'sans-serif'],
         },
       }
     },
